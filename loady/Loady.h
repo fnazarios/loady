fbf1b5d8a93dbd1b23b342a0bde38e242cdcf17f
Fernando Sousa <fnazarios@gmail.com>
2019-04-02 14:51:49 -0300

    Carthage Support

---
+++ loady/Loady.h
@@ -0,0 +1,19 @@
+//
+//  Loady.h
+//  Loady
+//
+//  Created by Fernando Sousa on 02/04/19.
+//  Copyright © 2019 farshadJahanmanesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Loady.
+FOUNDATION_EXPORT double LoadyVersionNumber;
+
+//! Project version string for Loady.
+FOUNDATION_EXPORT const unsigned char LoadyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loady/PublicHeader.h>
+
+
